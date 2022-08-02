64ab1573ad4fe27eb6fc1620fa77afa68248103c
David Ohiosimuan Ohimai <daveohimai@gmail.com>
2022-08-02 17:10:22 +0100

    Add-to-cart

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ContactComponent } from './navbar/contact/contact.component';
+import { LoginComponent } from './navbar/login/login.component';
+import { RegisterComponent } from './navbar/register/register.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'contact', component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './navbar/login/login.component';
+import { RegisterComponent } from './navbar/register/register.component';
+import { ContactComponent } from './navbar/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
